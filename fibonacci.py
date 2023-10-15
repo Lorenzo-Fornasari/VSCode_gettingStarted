fdf38af886fa94629e0f0c02cb63ed3ded5d5fab
Lorenzo Fornasari <lorenzoef@hotmail.com>
2023-10-14 21:32:07 -0300

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,14 @@ numTerms = int(input("How many terms? "))
 #  main method
 
 def fibonacci(n):
-    if n<1:
+    if n<=1:
         return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
 # check if the number of terms is valid
 
-if numTerms <= 0:
+if numTerms >= 0:
     print("Please enter a positive integer")
     for i in range(numTerms):
         print(fibonacci(i))
